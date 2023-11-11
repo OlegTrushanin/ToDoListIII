9f89280a73bd40095b74f463b0f949d469390d9a
Oleg Trushanin <o.trushanin@gmail.com>
2023-11-11 20:04:38 +0300

    убрал проблемы с AndroidSchedulers.mainThread() Класс disposable

--- app/src/main/java/com/example/todolistiii/AddViewNote.java
+++ app/src/main/java/com/example/todolistiii/AddViewNote.java
@@ -7,9 +7,14 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.functions.Action;
 import io.reactivex.rxjava3.schedulers.Schedulers;
+
+
 
 public class AddViewNote extends AndroidViewModel {
 
@@ -18,6 +23,8 @@ public class AddViewNote extends AndroidViewModel {
 
 
     private MutableLiveData <Boolean> shouldCloseScreen = new MutableLiveData<>();
+
+    private CompositeDisposable compositeDisposable = new CompositeDisposable(); // нужно чтобы удалить подписку discribeOn если сами уходим с активности
 
     public LiveData<Boolean> getShouldCloseScreen() {
         return shouldCloseScreen;
@@ -31,7 +38,7 @@ public class AddViewNote extends AndroidViewModel {
 
     public void saveNote(Note note){
 
-        dataBase.notesDao().add(note)
+       Disposable disposable = dataBase.notesDao().add(note)
                 .subscribeOn(Schedulers.io())// переключаем поток на фоновый для метода add
                 .observeOn(AndroidSchedulers.mainThread()) // переключаем обратно на главный поток. Все что ниже будет выполняться в главном потоке
                 .subscribe(new Action() {// с помощью этого метода подписываемся на add и отслеживаем окончание его работы добавляя колбек Action
@@ -40,8 +47,13 @@ public class AddViewNote extends AndroidViewModel {
                         shouldCloseScreen.setValue(true);
                     }
                 });
+       compositeDisposable.add(disposable);
 
     }
 
-
+    @Override
+    protected void onCleared() { // метод жизненного цикла View модели
+        super.onCleared();
+        compositeDisposable.dispose(); //удаляем все подписки
+    }
 }
